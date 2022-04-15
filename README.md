c140eb541075dfc9a1204bc9c25bc837695a7b05
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2022-04-14 21:11:39 -0700

    Made it into a python package.

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 # Termvideo
 
 Play videos in the terminal (stdout).
-
-`python main.py /path/to/video.mp4`
-
-Terminal size: `-x output_width`, `-y output_height`.
-Default autodetects and adapts if terminal is scaled.
-
-Warning: If you redirect output to a file, the file may become
-very large. Default behavior stops if redirecting. Add `--redirect` to allow.
-
-Add `--cpp` to compile and load a C++ library. This will greatly improve the speed.
-
-![](https://github.com/phuang1024/termvideo/blob/main/examples/piano.png?raw=true)
---
+++ setup.py
@@ -0,0 +1,52 @@
+#
+#  Termvideo
+#  Play videos in the terminal.
+#  Copyright Patrick Huang 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import setuptools
+
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+with open("requirements.txt", "r") as fp:
+    requirements = fp.read().strip().split("\n")
+
+setuptools.setup(
+    name="ffterm",
+    version="0.0.1",
+    author="Patrick Huang",
+    author_email="phuang1024@gmail.com",
+    description="Play media in the terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phuang1024/ffterm",
+    py_modules=["ffterm"],
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "ffterm = ffterm.main:main",
+        ]
+    },
+)
